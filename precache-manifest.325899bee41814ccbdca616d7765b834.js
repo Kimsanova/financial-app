6bf9485c8f90f1684568b5abe5f7d37a6e511e0b
Kimsanova <synak2013@gmail.com>
2020-04-04 03:00:13 +0600

    Updates

---
+++ precache-manifest.325899bee41814ccbdca616d7765b834.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e04d73fa74ca466dd718817f6bee3a5",
+    "url": "/financial-app/index.html"
+  },
+  {
+    "revision": "554dd3d83f6e755e32bb",
+    "url": "/financial-app/static/css/main.667f2a60.chunk.css"
+  },
+  {
+    "revision": "c9469a12ce2e97390e37",
+    "url": "/financial-app/static/js/2.1847da45.chunk.js"
+  },
+  {
+    "revision": "40f6ddb379fd6d5397eded99d3b7f8f9",
+    "url": "/financial-app/static/js/2.1847da45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554dd3d83f6e755e32bb",
+    "url": "/financial-app/static/js/main.c5a14fcf.chunk.js"
+  },
+  {
+    "revision": "ec40b6ad78d848b9c7d1",
+    "url": "/financial-app/static/js/runtime-main.25366e1e.js"
+  },
+  {
+    "revision": "09ecc732c85abc693621c4a8e1c4b5fe",
+    "url": "/financial-app/static/media/income.09ecc732.png"
+  },
+  {
+    "revision": "664ff3dcdd291b96f8894c207b35edb2",
+    "url": "/financial-app/static/media/spending.664ff3dc.png"
+  }
+]);
